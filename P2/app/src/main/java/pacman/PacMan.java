b5d4001bd2951840b95241b37306bc3f5b3fec67
Rohit Sengupta <rohitsengupta@wireless-10-104-168-56.umd.edu>
2023-03-29 13:15:35 -0400

    pushing rohit fixes

--- P2/app/src/main/java/pacman/PacMan.java
+++ P2/app/src/main/java/pacman/PacMan.java
@@ -37,20 +37,20 @@ public class PacMan {
     int choice = (int) (Math.random() * validMoves.size());
 
     if (validMoves.size() == 0 ||
-        myMap.move(myName, validMoves.get(choice), Map.Type.PACMAN))
+        !myMap.move(myName, validMoves.get(choice), Map.Type.PACMAN))
       return false;
 
     this.myLoc = validMoves.get(choice);
-    return false;
+    return true;
   }
 
   public boolean is_ghost_in_range() {
     for (int dx = -1; dx <= 1; dx++) {
       for (int dy = -1; dy <= 1; dy++) {
-        Location newLocation = myLoc.shift(dy, dx);
+        Location newLocation = myLoc.shift(dx, dy);
 
-        if (myMap.getLoc(newLocation).contains(Map.Type.PACMAN))
-          return false;
+        if (myMap.getLoc(newLocation).contains(Map.Type.GHOST))
+          return true;
       }
     }
 
